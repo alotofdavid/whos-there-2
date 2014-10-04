72e3f2460b5de0d97ebcfe23f838cb68f01f0a91
alotofdavid <david.adams94@gmail.com>
2014-10-04 04:33:34 -0700

    friends list

---
+++ whosthere/FriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsViewController.h
+//  whosthere
+//
+//  Created by David Adams on 10/4/14.
+//  Copyright (c) 2014 Weston Mizumoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsViewController : UIViewController
+
+@end
